0176ff914c2f53e53a4fbcca86383b59447186ed
frbend <62542322+frbend@users.noreply.github.com>
2024-04-08 16:42:12 +0200

    Změna Ajax požadavku
    Pokud je input ve špatném formátu (3.5 atd.) stránka vyšle odkaz a server odpoví s chybovou zprávou na stránce

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 $(document).ready(function() {
-    // handleButtonClick funkce. Omezuje textové pole od 1 do 100
+    // Funkce handleButtonClick. Omezuje textové pole od 1 do 100
     function handleButtonClick() {
         var rowNumber = $('#rowNumber').val(); // Získá hodnotu z textového pole
 
-        //Kontrola zda je hodnota z textového pole mezi 1 a 100
+        // Kontrola zda je hodnota z textového pole mezi 1 a 100
         if (rowNumber < 1 || rowNumber > 100) {
-            //Zobrazí popup zprávu
+            // Zobrazí popup zprávu
             alert("Limit textového pole je mezi 1 a 100");
 
             //Změní hodnotu textového pole na nejbližší možné číslo - cokoliv nad 100 změní na 100
             $('#rowNumber').val(Math.min(Math.max(parseInt(rowNumber), 1), 100));
 
-            // Return to prevent further processing
+            // Return pro zabránění dalšího zpracování
             return;
         }
 
-        // AJAX request
+        // AJAX požadavek
         $.ajax({
             url: '../index.php',
             type: 'POST',
-            data: { rowNumber: rowNumber },   //Předá hodnotu textového pole do PHP skriptu
-            dataType: 'json',  //Očekává data ve formátu JSON
+            data: { rowNumber: rowNumber },   // Předá hodnotu z textového pole do PHP skriptu
+            dataType: 'json',  // Očekává JSON data jako odpověď
             success: function(response) {
                 console.log(response);  //Hodnota odpověďi ve formátu JSON v console logu
                 //Vymaže předchozí výsledek
@@ -52,13 +52,13 @@ $(document).ready(function() {
         });
     }
 
-    //Event listener pro click tlačítka
+    // Event listener pro kliknutí na tlačítko
     $('#testButton').click(handleButtonClick);
 
-    //Event listener pokud se zmáčkne enter
+    // Event listener pro stisknutí klávesy Enter
     $('#rowNumber').keypress(function(event) {
-        if (event.which === 13) { // Check if Enter key was pressed
-            event.preventDefault(); // Prevent default form submission
-            handleButtonClick(); // Call the function to handle button click
+        if (event.which === 13) { // Kontrola, zda byla stisknuta klávesa Enter
+            event.preventDefault(); // Zabránění výchozímu odeslání formuláře
+            handleButtonClick(); // Zavolání funkce pro zpracování kliknutí na tlačítko
         }
     })});
